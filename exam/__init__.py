2a99aff5fbcf51914bc9ad23fa83281f3b3b1f58
yuxian158 <yuxian158@gmail.com>
2020-06-19 12:25:35 +0800

    xiufu

--- exam/__init__.py
+++ exam/__init__.py
@@ -2,10 +2,8 @@ import re
 
 
 def ser(ch: str) -> str:
-    with open("dome.txt", 'r', encoding='utf8') as f:
+    with open("exam\dome.txt", 'r', encoding='utf8') as f:
         read = f.readlines()
         for lin in read:
             if re.match(".*" + ch + ".*", lin):
                 return lin
-
-
